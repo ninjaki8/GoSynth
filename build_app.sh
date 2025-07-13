8b88990bad39b4ccef664bd7960e122e055fb890
ninjaki8 <t.andreou83@gmail.com>
2025-07-13 13:41:47 +0300

    updated build.sh and compiled latest build

--- build_app.sh
+++ build_app.sh
@@ -2,13 +2,10 @@
 set -e
 
 echo "Building for Linux..."
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o build/goSynth-linux
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o build/GoSynth-linux
 
 echo "Building for Windows..."
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o build/goSynth.exe
-
-echo "Building for macOS..."
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o build/goSynth-macos
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o build/GoSynth.exe
 
 echo "Done!"
 
